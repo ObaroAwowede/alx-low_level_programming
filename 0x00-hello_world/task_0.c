69d8884c85a6558477aa299d25c2e68c58509185
ObaroAwowede <akpobaroawowede@gmail.com>
2022-10-13 05:18:50 -0700

    corrected in line with becky and made script executable

--- 0x00-hello_world/task_0.c
+++ 0x00-hello_world/task_0.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'first task'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
- printf("Hello World\n");
- return (0);
+printf("Hello World\n");
+return (0);
 
-} 
-
-
+}
