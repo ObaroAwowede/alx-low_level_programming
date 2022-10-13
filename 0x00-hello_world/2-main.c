0242cd1df9758cc49c97dd27cd250d446b7c7ffd
ObaroAwowede <akpobaroawowede@gmail.com>
2022-10-13 06:47:27 -0700

    done task 3, used betty to correct the c files

--- 0x00-hello_world/2-main.c
+++ 0x00-hello_world/2-main.c
@@ -1,15 +1,13 @@
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+#include <stdio.h>
 
+/**                                                                                                                                     
+ * main - Entry point
+ *
+ * Description: 'second task'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
 
  return (0);
