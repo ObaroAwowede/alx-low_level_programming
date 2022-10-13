2bfaf665132645c8a5fd59e167b40ae346520347
ObaroAwowede <akpobaroawowede@gmail.com>
2022-10-13 07:19:18 -0700

    done task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'fourth task'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char hello[] = ""Programming is like building a multilingual puzzle\n";
+	puts(hello);
+	return (0);
+
+}
