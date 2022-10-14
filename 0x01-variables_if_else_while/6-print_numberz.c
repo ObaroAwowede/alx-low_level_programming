5cac19d9d2fec6d25528b1381f658100694fb5d0
ObaroAwowede <akpobaroawowede@gmail.com>
2022-10-14 07:36:09 -0700

    done tasks 4-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Task six'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
