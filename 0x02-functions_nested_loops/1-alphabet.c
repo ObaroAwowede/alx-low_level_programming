977fa25c1e9a8861d55360d4e5b93b6d7b78e03c
ObaroAwowede <akpobaroawowede@gmail.com>
2022-10-18 04:11:17 -0700

    ndone task 1 issues with main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int q;
+
+	for (q = 'a'; q <= 'z'; q++)
+
+	{
+		_putchar(q);
+	}
+	_putchar('\n');
+}
